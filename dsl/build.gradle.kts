b223d65fefceb36acfc227c722a317cb604e7a1e
Mark <mekosichkin@gmail.com>
2023-03-25 15:38:24 +0300

    Migrate dsl to multiplatform

--- dsl/build.gradle.kts
+++ dsl/build.gradle.kts
@@ -1,11 +1,22 @@
-dependencies {
-    implementation(kotlin("stdlib"))
-    api(project(":core:jdbc"))
-    testImplementation("io.kotest:kotest-assertions-core:5.2.2")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
-    testImplementation("io.mockk:mockk:1.12.3")
+kotlin {
+    jvm() {
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib"))
+                api(project(":core:jdbc"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation("io.kotest:kotest-assertions-core:5.2.2")
+                implementation("org.junit.jupiter:junit-jupiter:5.8.2")
+                implementation("io.mockk:mockk:1.12.3")
+            }
+        }
+    }
 }
-
 tasks.withType<Test> {
     useJUnitPlatform()
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,8 @@ rootProject.name = "yasb"
 include(
     "core",
     "core:jdbc",
-    "core:jdbc:postgres"
-//    "dsl",
+    "core:jdbc:postgres",
+    "dsl"
 //    "dsl:postgres",
 //    "dsl:generator",
 //    "dsl:generator:flyway",
